4ba9e1a5e53595e9922d36f95467fe1074f95424
ifunkenstein <funkensteinif@outlook.com>
2024-11-18 20:48:00 +0100

    resimplified interaction trigger

--- Assets/04_Code/CharacterInteractionTrigger.cs
+++ Assets/04_Code/CharacterInteractionTrigger.cs
@@ -3,40 +3,28 @@ using UnityEngine;
 public class CharacterInteractionTrigger : MonoBehaviour
 {
     [Header("Interaction Settings")]
-    public Transform player;
-    public Transform character;
-    public float interactionDistance = 3f;
-    public Vector3 playerTargetPosition;
+    public Collider2D levelCollider;
 
-    private DialogueManager dialogueManager;
+   // private DialogueManager dialogueManager;
     private bool isInteracting = false;
 
     private void Start()
     {
-        // Find the DialogueManager in the scene
-        dialogueManager = FindObjectOfType<DialogueManager>();
+            Debug.Log(GetComponent<Collider2D>().name);
     }
 
     private void Update()
     {
-        // Check if characters are within interaction distance
-        float distance = Vector2.Distance(player.position, character.position);
-            //Debug.Log(distance);
-
-
-        if (distance <= interactionDistance && Input.GetKeyDown(KeyCode.Space) && !isInteracting) // && !dialogueManager.IsInDialogue)
-        {
-            isInteracting = true;
-            MovePlayerToPosition();
-            Debug.Log("talk");    
-        }
-                         
+                                 
+    }
+    private void OnTriggerEnter2D(Collider2D playerCollision) 
+    {
+        Debug.Log(levelCollider.name);
     }
 
     private void MovePlayerToPosition()
     {
-        player.position = playerTargetPosition;
-       // dialogueManager.StartDialogue();
+    
     }
 }
 
